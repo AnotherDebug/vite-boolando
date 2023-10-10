e0f63e561019e4855df24f4f2d78fe6415e2d5ed
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-10 15:20:36 +0200

    [INIT]: vite e vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+name: "App"
+}
+</script>
+
+
+<template>
+ <div>
+  <h1>Boolando</h1>
+ </div>
+</template>
+
+
+
+<style lang="scss">
+
+@use "./scss/main.scss";
+
+</style>
