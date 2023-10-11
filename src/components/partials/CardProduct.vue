e257e971516e4a921c2b06aa65cdafa02d17e4d7
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-11 16:22:12 +0200

    [ADD]: v if e class binding

--- src/components/partials/CardProduct.vue
+++ src/components/partials/CardProduct.vue
@@ -31,11 +31,11 @@ export default {
             <p>&hearts;</p>
         </div>
         <p v-if="products.discount !== null" class="discount">{{ products.discount }}</p>
-        <p v-if="products.sostenibilita === true" class="sustain">Sostenibilità</p>
+        <p v-if="products.sostenibilita === true" class="sustain" :class=" products.discount === null && products.sostenibilita === true ? 'sus-only' : '' ">Sostenibilità</p>
         <p class="trademark">{{ products.marca }}</p>
         <p class="description text-strong">{{ description }}</p>
         <span class="price-discount text-red">{{ priceDiscount }}</span>
-        <span class="price">{{ priceFull }}</span>
+        <span v-if="this.products.fullPrice !== null" class="price">{{ priceFull }}</span>
     </section>
 </template>
 
@@ -122,13 +122,14 @@ export default {
         color: $white-color;
     }
 
-    #sus-only {
+    .sus-only {
         position: absolute;
         left: 5px;
     }
 
     .price {
         text-decoration: line-through;
+        margin-left: 10px;
     }
 }
 </style>
