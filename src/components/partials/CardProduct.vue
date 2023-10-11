6bc34a736b8dfd1d181f406e198fdcff4813de10
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-11 15:43:41 +0200

    [ADD]: methods e v-if

--- src/components/partials/CardProduct.vue
+++ src/components/partials/CardProduct.vue
@@ -1,126 +1,134 @@
 <script>
 export default {
     name: "CardProduct",
-    props:{
-        products:Object
+    props: {
+        products: Object
+    },
+    computed: {
+        description() {
+            return this.products.modello.toUpperCase();
+        },
+        priceDiscount() {
+            return `${this.products.lastPrice} €`;
+        },
+        priceFull() {
+            return `${this.products.fullPrice} €`;
+        }
     }
+
 }
 </script>
 
 
 <template>
-
     <!-- Singola card -->
     <section class="card">
-            <div class="img">
-                <img class="first" src="/src/assets/img/1.webp" alt="">
-                <img class="second" src="/src/assets/img/1b.webp" alt="">
-            </div>
-            <div class="heart">
-                <p>&hearts;</p>
-            </div>
-            <p class="discount">-50%</p>
-            <p class="sustain">Sostenibilità</p>
-            <p class="trademark">Levi's</p>
-            <p class="description text-strong">RELAXED FIT TEE UNISEX</p>
-            <span class="price-discount text-red">14,99 <span>&euro;</span></span>
-            <span class="price">29,99 <span>&euro;</span></span>
-        </section>
-  
+        <div class="img">
+            <img class="first" :src="`/src/assets/img/${products.primaryImage}`" alt="">
+            <img class="second" :src="`/src/assets/img/${products.secondaryImage}`" alt="">
+        </div>
+        <div class="heart">
+            <p>&hearts;</p>
+        </div>
+        <p v-if="products.discount !== null" class="discount">{{ products.discount }}</p>
+        <p v-if="products.sostenibilita === true" class="sustain">Sostenibilità</p>
+        <p class="trademark">{{ products.marca }}</p>
+        <p class="description text-strong">{{ description }}</p>
+        <span class="price-discount text-red">{{ priceDiscount }}</span>
+        <span class="price">{{ priceFull }}</span>
+    </section>
 </template>
 
 
 <style lang="scss" scoped>
-
 @use "../../scss/partials/variables" as *;
 
 .card {
-        width: 33%;
-        min-width: 200px;
-        display: inline-block;
-        margin-top: 50px;
-        position: relative;
-        font-size: 12px;
-        padding: 5px;
+    width: 33%;
+    min-width: 200px;
+    display: inline-block;
+    margin-top: 50px;
+    position: relative;
+    font-size: 12px;
+    padding: 5px;
 
-        img {
-            width: 100%;
-            cursor: pointer;
+    img {
+        width: 100%;
+        cursor: pointer;
+    }
+
+    .img {
+        .first {
+            display: block;
         }
 
-        .img {
-            .first {
-                display: block;
-            }
-
-            .second {
-                display: none;
-            }
-
-            &:hover .first {
-                display: none;
-            }
-
-            &:hover .second {
-                display: block;
-            }
+        .second {
+            display: none;
         }
 
-        .heart {
-            width: 40px;
-            height: 40px;
-            background-color: $white-color;
-            color: black;
-            text-align: center;
-            line-height: 40px;
-            font-size: 25px;
-            position: absolute;
-            top: 2%;
-            right: 0;
-            cursor: pointer;
-
-            &:hover {
-                color: $red-color;
-            }
+        &:hover .first {
+            display: none;
         }
 
-        .discount {
-            background-color: $red-color;
-            display: inline-block;
-            padding: 5px 10px;
-            position: absolute;
-            left: 5px;
-            bottom: 15%;
-            font-weight: 600;
-            color: $white-color;
-        }
-
-        .trademark,
-        .price,
-        .price-discount {
-            font-size: 10px;
-            padding-top: 5px;
-        }
-
-        .sustain {
-            display: inline-block;
-            padding: 5px 10px;
-            background-color: $green-color;
-            position: absolute;
-            left: 14%;
-            bottom: 15%;
-            font-weight: 600;
-            color: $white-color;
-        }
-
-        #sus-only {
-            position: absolute;
-            left: 5px;
-        }
-
-        .price {
-            text-decoration: line-through;
+        &:hover .second {
+            display: block;
         }
     }
 
+    .heart {
+        width: 40px;
+        height: 40px;
+        background-color: $white-color;
+        color: black;
+        text-align: center;
+        line-height: 40px;
+        font-size: 25px;
+        position: absolute;
+        top: 2%;
+        right: 0;
+        cursor: pointer;
+
+        &:hover {
+            color: $red-color;
+        }
+    }
+
+    .discount {
+        background-color: $red-color;
+        display: inline-block;
+        padding: 5px 10px;
+        position: absolute;
+        left: 5px;
+        bottom: 15%;
+        font-weight: 600;
+        color: $white-color;
+    }
+
+    .trademark,
+    .price,
+    .price-discount {
+        font-size: 10px;
+        padding-top: 5px;
+    }
+
+    .sustain {
+        display: inline-block;
+        padding: 5px 10px;
+        background-color: $green-color;
+        position: absolute;
+        left: 14%;
+        bottom: 15%;
+        font-weight: 600;
+        color: $white-color;
+    }
+
+    #sus-only {
+        position: absolute;
+        left: 5px;
+    }
+
+    .price {
+        text-decoration: line-through;
+    }
+}
 </style>
