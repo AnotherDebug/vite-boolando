c2593de3bc1f63f6d1677e087061959a27f5efaa
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-11 14:25:42 +0200

    [ADD]: id product

--- src/data/products.js
+++ src/data/products.js
@@ -1,5 +1,6 @@
 export default [
     {
+      id:1548,
       primaryImage: '1.webp',
       secondaryImage: '1b.webp',
       discount: '-50%',
@@ -10,6 +11,7 @@ export default [
       lastPrice: 14.99
     },
     {
+      id:51548,
       primaryImage: '2.webp',
       secondaryImage: '2b.webp',
       discount: '-30%',
@@ -20,6 +22,7 @@ export default [
       lastPrice: 20.99
     },
     {
+      id:188763,
       primaryImage: '3.webp',
       secondaryImage: '3b.webp',
       discount: '-30%',
@@ -30,6 +33,7 @@ export default [
       lastPrice: 129.99
     },
     {
+      id:32182,
       primaryImage: '4.webp',
       secondaryImage: '4b.webp',
       discount: '-50%',
@@ -40,6 +44,7 @@ export default [
       lastPrice: 29.99
     },
     {
+      id:1535048,
       primaryImage: '5.webp',
       secondaryImage: '5b.webp',
       discount: '-50%',
@@ -50,6 +55,7 @@ export default [
       lastPrice: 99.99
     },
     {
+      id:1540218,
       primaryImage: '6.webp',
       secondaryImage: '6b.webp',
       discount: null,
